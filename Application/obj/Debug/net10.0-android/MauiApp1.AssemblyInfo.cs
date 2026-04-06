5dd6c065bdcafb60ac85ea46009bfcdbe96f08dd
LuDaddy1509 <luhocnhan@gmail.com>
2026-04-06 09:16:37 +0700

    update sql and map

--- Application/obj/Debug/net10.0-android/MauiApp1.AssemblyInfo.cs
+++ Application/obj/Debug/net10.0-android/MauiApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e4111678c994fec2b8bfca0046e1a00f281f3587")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4e1e07f03c336ca52ceab5989057603a2c2f0784")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MapApi/obj/Debug/net10.0/MapApi.AssemblyInfo.cs
+++ MapApi/obj/Debug/net10.0/MapApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MapApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4111678c994fec2b8bfca0046e1a00f281f3587")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e1e07f03c336ca52ceab5989057603a2c2f0784")]
 [assembly: System.Reflection.AssemblyProductAttribute("MapApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MapApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
