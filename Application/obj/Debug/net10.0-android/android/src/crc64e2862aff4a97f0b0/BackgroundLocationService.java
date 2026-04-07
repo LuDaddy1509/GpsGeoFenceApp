bacbfde9a4821436c21b7bb9ad391aa09b7fb9a4
LuDaddy1509 <luhocnhan@gmail.com>
2026-04-07 21:25:21 +0700

    update lang and sql

---
+++ Application/obj/Debug/net10.0-android/android/src/crc64e2862aff4a97f0b0/BackgroundLocationService.java
@@ -0,0 +1,70 @@
+package crc64e2862aff4a97f0b0;
+
+
+public class BackgroundLocationService
+	extends android.app.Service
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:()V:GetOnCreateHandler\n" +
+			"n_onStartCommand:(Landroid/content/Intent;II)I:GetOnStartCommand_Landroid_content_Intent_IIHandler\n" +
+			"n_onBind:(Landroid/content/Intent;)Landroid/os/IBinder;:GetOnBind_Landroid_content_Intent_Handler\n" +
+			"n_onDestroy:()V:GetOnDestroyHandler\n" +
+			"";
+		mono.android.Runtime.register ("MauiApp1.Platforms.Android.BackgroundLocationService, MauiApp1", BackgroundLocationService.class, __md_methods);
+	}
+
+	public BackgroundLocationService ()
+	{
+		super ();
+		if (getClass () == BackgroundLocationService.class) {
+			mono.android.TypeManager.Activate ("MauiApp1.Platforms.Android.BackgroundLocationService, MauiApp1", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+	public void onCreate ()
+	{
+		n_onCreate ();
+	}
+
+	private native void n_onCreate ();
+
+	public int onStartCommand (android.content.Intent p0, int p1, int p2)
+	{
+		return n_onStartCommand (p0, p1, p2);
+	}
+
+	private native int n_onStartCommand (android.content.Intent p0, int p1, int p2);
+
+	public android.os.IBinder onBind (android.content.Intent p0)
+	{
+		return n_onBind (p0);
+	}
+
+	private native android.os.IBinder n_onBind (android.content.Intent p0);
+
+	public void onDestroy ()
+	{
+		n_onDestroy ();
+	}
+
+	private native void n_onDestroy ();
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
